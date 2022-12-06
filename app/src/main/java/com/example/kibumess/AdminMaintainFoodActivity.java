b71b4c2bcac2543cc80579cc98c4e971217bc1d0
Ochieng254 <kevinochiengseda2020@gmail.com>
2022-12-06 13:39:11 +0300

    first commit

---
+++ app/src/main/java/com/example/kibumess/AdminMaintainFoodActivity.java
@@ -0,0 +1,14 @@
+package com.example.kibumess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminMaintainFoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_maintain_food);
+    }
+}
